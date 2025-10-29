c76e1a6f5a1cdcb3deea46948841db1d0d6ecfb4
naik0055 <naik0055@umn.edu>
2025-10-29 14:45:37 -0500

    Updated getUrls.py
    updated getUrls so that it gets all of the individual apartment floor plan pages on apartments.com

--- getUrls.py
+++ getUrls.py
@@ -2,8 +2,9 @@ import httpx
 from selectolax.parser import HTMLParser
 
 def getUrls():
-    BasewebsiteUrl = "https://www.apartments.com/off-campus-housing/mn/minneapolis/university-of-minnesota/student-housing-per-person/"
+    BasewebsiteUrl = "https://www.apartments.com/off-campus-housing/mn/minneapolis/university-of-minnesota/"
     headers = {"User-Agent":"iTunes/12.13 (Windows; Microsoft Windows 10 x64; x64) AppleWebKit/7613.2007"}
+    
 
     websiteUrls = [BasewebsiteUrl]
     i = 1
@@ -13,7 +14,7 @@ def getUrls():
 
     urls = []
 
-    for websiteUrl in websiteUrls:
+    for websiteUrl in websiteUrls[0:5]:
         resp = httpx.get(websiteUrl, headers=headers, timeout=10)
         html = HTMLParser(resp.text)
 
@@ -24,4 +25,17 @@ def getUrls():
             if url is not None and url not in urls:
                 urls.append(url)
 
-    return urls
+    tempUrls = []
+    finalUrls = []
+
+    for url in urls:
+        resp = httpx.get(url, headers=headers, timeout=10)
+        html = HTMLParser(resp.text)
+        roomInfo = html.css("li.unitContainer.js-unitContainerV3")
+        for room in roomInfo:
+            tempUrls.append(room.attributes.get("data-unitkey") + "-2-unit")
+        
+        for tempUrl in tempUrls:
+            finalUrls.append(url + "#" + tempUrl)
+
+    return finalUrls
