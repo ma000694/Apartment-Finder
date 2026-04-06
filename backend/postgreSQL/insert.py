e7335b8db99e67e7c9b88225edb16dae13b733c5
Aaron Ma <aaronma2006@gmail.com>
2026-04-06 18:40:09 -0500

    postgreSQL insert function

--- backend/postgreSQL/insert.py
+++ backend/postgreSQL/insert.py
@@ -12,7 +12,7 @@ conn = psycopg2.connect(
 
 cur = conn.cursor() # create cursor
 
-# create table if doesn't exist already
+# create properties table if doesn't exist already
 cur.execute("""
 CREATE TABLE IF NOT EXISTS properties(
     id SERIAL PRIMARY KEY,
@@ -29,15 +29,92 @@ CREATE TABLE IF NOT EXISTS properties(
 )
 """)
 
-# open csv file and copy into postgresql
-with open("umn_apartment_data.csv", "r") as f:
+# create temporary staging table in order to filter duplicates
+cur.execute("""
+CREATE TEMP TABLE staging_table(
+    id SERIAL PRIMARY KEY,
+    building_name TEXT,
+    unit_name TEXT,
+    address TEXT,
+    beds SMALLINT,
+    baths SMALLINT,
+    rent INTEGER,
+    sqft INT,
+    availability DATE,
+    url TEXT,
+    amenities TEXT
+)
+""")
+
+# open csv file and copy into temp table
+with open("test_insert.csv", "r") as f:
     cur.copy_expert(
         """
-        COPY properties(building_name,unit_name,address,beds,baths,rent,sqft,availability,url,amenities)
+        COPY staging_table(building_name,unit_name,address,beds,baths,rent,sqft,availability,url,amenities)
         FROM STDIN WITH CSV HEADER
         """,
         f
     )
+
+# set conflict constraint to determine duplicates
+# executes anonymous code block and ensure that constraint doesn't already exist
+cur.execute("""
+    DO $$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT 1 FROM pg_constraint
+            WHERE conname = 'unique_property'
+        ) THEN
+            ALTER TABLE properties
+            ADD CONSTRAINT unique_property UNIQUE (url);
+        END IF;
+    END $$;
+""")
+
+# upsert from temp table into properties table
+# if there are conflicts, update the row instead of making a new one
+cur.execute("""
+    INSERT INTO properties (
+    building_name, unit_name, address, beds, baths,
+    rent, sqft, availability, url, amenities
+    )
+    SELECT
+        building_name, unit_name, address, beds, baths,
+        rent, sqft, availability, url, amenities
+    FROM staging_table
+    ON CONFLICT (url)
+    DO UPDATE SET
+        building_name = EXCLUDED.building_name,
+        unit_name = EXCLUDED.unit_name,
+        address = EXCLUDED.address,
+        beds = EXCLUDED.beds,
+        baths = EXCLUDED.baths,
+        rent = EXCLUDED.rent,
+        sqft = EXCLUDED.sqft,
+        availability = EXCLUDED.availability,
+        amenities = EXCLUDED.amenities
+    WHERE (
+        properties.building_name,
+        properties.unit_name,
+        properties.address,
+        properties.beds,
+        properties.baths,
+        properties.rent,
+        properties.sqft,
+        properties.availability,
+        properties.amenities
+    ) IS DISTINCT FROM (
+        EXCLUDED.building_name,
+        EXCLUDED.unit_name,
+        EXCLUDED.address,
+        EXCLUDED.beds,
+        EXCLUDED.baths,
+        EXCLUDED.rent,
+        EXCLUDED.sqft,
+        EXCLUDED.availability,
+        EXCLUDED.amenities
+    );
+""")
 
 cur.execute("SELECT * FROM properties")
 rows = cur.fetchall()
